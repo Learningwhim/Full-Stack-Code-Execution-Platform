9ced1fa0d0c97b4929e80b86e6002b817b049313
awwyushh <swapnilambad80@gmail.com>
2025-08-28 19:08:07 +0530

    changed ports

--- backend/myapp/database.js
+++ backend/myapp/database.js
@@ -1,4 +1,6 @@
+
 const knex = require('knex');
 const knex_config = require('./knexfile.js');
-export_devObj = knex(knex_config.development);
-module.exports = export_devObj;
+const db = knex(knex_config.development);
+
+module.exports = db;
