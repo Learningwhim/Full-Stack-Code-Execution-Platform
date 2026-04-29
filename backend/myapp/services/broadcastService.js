3d472fe401f2c87ca7240258a31caaeb8903ea5d
Swapnil <swapnilambad80@gmail.com>
2026-04-29 17:06:29 +0530

    broastacst debug check 2

--- backend/myapp/services/broadcastService.js
+++ backend/myapp/services/broadcastService.js
@@ -5,7 +5,8 @@ async function triggerLeaderboardBroadcast(roomCode, newLeaderboardData){
         console.log("Service working");
         console.log("EMITTING TO ROOM:", roomCode);
         //console.log(roomCode, newLeaderboardData);
-        io.to(roomCode).emit('leaderboardUpdate', newLeaderboardData);
+        //io.to(roomCode).emit('leaderboardUpdate', newLeaderboardData);
+        io.emit('leaderboardUpdate', newLeaderboardData);
     }catch(error){
         throw error;
     }
