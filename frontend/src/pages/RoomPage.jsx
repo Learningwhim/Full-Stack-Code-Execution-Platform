c7a70fbba2ffa3768f57757b055921af93a1154a
Swapnil <swapnilambad80@gmail.com>
2026-05-03 12:29:14 +0530

    lb socket logic shortening trial

--- frontend/src/pages/RoomPage.jsx
+++ frontend/src/pages/RoomPage.jsx
@@ -132,36 +132,10 @@ function RoomPage() {
             // });
             
             socket.on('leaderboardUpdate', (data) => {
-            console.log("Leaderboard update received!", data);
+              console.log("Leaderboard update received!", data);
 
-            setRoomParticipants(prev => {
-          const map = new Map(
-            prev.map(p => [p.user_id, p])
-          );
-
-          data.forEach(p => {
-            const id = p.user_id;
-            if (!id) return;
-
-            const old = map.get(id) || {};
-
-            map.set(id, {
-              ...old,
-
-              participant_id: old.participant_id ?? p.participant_id,
-              user_id: id,
-
-              email: old.email,
-              name: old.name,
-
-              score: p.score ?? old.score ?? 0,
-              total_time: p.total_time ?? old.total_time ?? null,
+              setRoomParticipants(data);   // 🔥 FULL REPLACE
             });
-          });
-
-          return Array.from(map.values());
-        });
-          });
             return () => {
                 socket.disconnect();
             }
